bc11c01dd6a736c595b1a47453c9107217ce36cb
Hansu Lee <bunnybrewery@gmail.com>
2024-10-05 17:09:46 +0900

    updated with docker

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run --name reminder-nginx -d -p 80:8080 -p 443:8443 custom-nginx
+# docker run --name reminder-nginx -d -p 80:8080 -p 443:8443 custom-nginx
+
+docker run -d \
+  --name reminder-nginx \
+  --network reminder_network \
+  -p 443:8443 \
+  custom-nginx
